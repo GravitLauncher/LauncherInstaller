a9658898ffb311d01a1bfc4c42fa47f82a98e2c9
sasha0552 <admin@sasha0552.ru>
2019-05-23 03:01:21 +0600

    Билд дев

--- src/main.cpp
+++ src/main.cpp
@@ -9,81 +9,26 @@ bool is_debian = false;
 bool is_arch = false;
 bool is_centos = false;
 bool is_root = false;
-
-using namespace std;
-
-string download_url = "http://mirror.gravitlauncher.ml/build/";
-
+std::string download_url = "http://mirror.gravitlauncher.ml/build/";
 bool is_file_exist(const char* file)
 {
-    struct stat sts;
-    return !((stat (file, &sts)) == -1);
+	struct stat sts;
+	return !((stat(file, &sts)) == -1);
 }
-
 bool read_yes()
 {
-    string response;
-    cin >> response;
-    return (response == "yes" || response == "YES" || response == "Yes");
+	std::string response;
+	std::cin >> response;
+	return (response == "yes" || response == "YES" || response == "Yes");
 }
-
-int main()
-{
-	cout << "GravitLauncher Linux installer" << endl;
-	if (is_file_exist("/bin/apt-get"))
-	{
-		is_debian = true;
-	}
-	if (is_file_exist("/bin/pacman"))
-	{
-		is_arch = true;
-	}
-	if (is_file_exist("/bin/yum"))
-	{
-		is_centos = true;
-	}
-	if (getuid() == 0)
-	{
-		if (root_action() != 0) return -1;
-	}
-	cout << "Avalible versions: [dev, 5.0.0b6, 5.0.0b7]" << endl;
-	cout << "Print version:";
-	string version;
-	cin >> version;
-	if (version == "dev" || version == "5.0.0b6" || version == "5.0.0b7")
-	{
-		if (version == "dev") {
-			buildDev();
-		}
-		download_url = download_url + version;
-	}
-	else
-	{
-		cout << "Version " << version << " not found. Download dev[YES/NO]?";
-		if (read_yes())
-		{
-			download_url = download_url + "dev";
-		}
-		else return 0;
-	}
-	if (install_action() != 0) return -1;
-	if (is_root)
-	{
-		if (is_file_exist("/bin/bash"))
-		{
-			execl("/bin/bash", NULL);
-		}
-	}
-}
-
 int root_action()
 {
-	cerr << "[WARN] Installing on behalf of the superuser is prohibited" << endl;
+	std::cerr << "[WARN] Installing on behalf of the superuser is prohibited" << std::endl;
 	is_root = true;
-	cout << "[ROOT] Check package installation" << endl;
+	std::cout << "[ROOT] Check package installation" << std::endl;
 	if (!is_file_exist("/bin/unzip") && (is_debian || is_arch || is_centos))
 	{
-		cout << "[ROOT] Unzip not found. Install [YES/NO]? ";
+		std::cout << "[ROOT] Unzip not found. Install [YES/NO]? ";
 		if (read_yes())
 		{
 			if (is_debian)
@@ -102,7 +47,7 @@ int root_action()
 	}
 	if (!is_file_exist("/bin/curl") && (is_debian || is_arch || is_centos))
 	{
-		cout << "[ROOT] Curl not found. Install [YES/NO]? ";
+		std::cout << "[ROOT] Curl not found. Install [YES/NO]? ";
 		if (read_yes())
 		{
 			if (is_debian)
@@ -121,12 +66,12 @@ int root_action()
 	}
 	if (!is_file_exist("/bin/git") && (is_debian || is_arch || is_centos))
 	{
-		cout << "[ROOT] Git not found. Install [YES/NO]? ";
+		std::cout << "[ROOT] git not found. Install [YES/NO]? ";
 		if (read_yes())
 		{
 			if (is_debian)
 			{
-				system("apt install git");
+				system("apt-get install git");
 			}
 			else if (is_arch)
 			{
@@ -138,15 +83,15 @@ int root_action()
 			}
 		}
 	}
-	string response;
-	cout << "Create new user for LaunchServer [YES/NO]? ";
-	cin >> response;
+	std::string response;
+	std::cout << "Create new user for LaunchServer [YES/NO]? ";
+	std::cin >> response;
 	if (response == "yes" || response == "YES" || response == "Yes")
 	{
 		system("useradd -u 2001 -m launchserver");
-		cout << "[ROOT] Create user launchserver" << endl;
+		std::cout << "[ROOT] Create user launchserver" << std::endl;
 		chdir("/home/launchserver");
-		cout << "[ROOT] Change dir to /home/launchserver" << endl;
+		std::cout << "[ROOT] Change dir to /home/launchserver" << std::endl;
 		setuid(2001);
 	}
 	else
@@ -155,16 +100,171 @@ int root_action()
 	}
 	return 0;
 }
+int install_action()
+{
+	std::cout << "UID: " << getuid() << "" << std::endl;
+	std::cout << "Download LaunchServer" << std::endl;
+	std::string download_launchserver = "curl -o LaunchServer.jar " + download_url + "/LaunchServer.jar";
+	system(download_launchserver.c_str());
+	if (!is_file_exist("LaunchServer.jar"))
+	{
+		std::cerr << "[ERROR] LaunchServer.jar not found. May be curl not installed?";
+		return -1;
+	}
+	std::cout << "Download libraries" << std::endl;
+	std::string download_lib = "curl -o libraries.zip " + download_url + "/libraries.zip";
+	system(download_lib.c_str());
+	if (!is_file_exist("libraries.zip"))
+	{
+		std::cerr << "[ERROR] libraries.zip not found. May be curl not installed?";
+		return -1;
+	}
+	std::cout << "Unpack libraries" << std::endl;
+	system("unzip libraries.zip");
+	if (!is_file_exist("libraries"))
+	{
+		std::cerr << "[ERROR] libraries not found. May be unzip not installed?";
+		return -1;
+	}
+	system("rm libraries.zip");
+	test();
+}
+
+int test() {
+	std::cout << "Installation test" << std::endl;
+	system("java -javaagent:LaunchServer.jar -jar LaunchServer.jar multi build checkInstall stop");
+	std::cout << "Chmod private.key to 700" << std::endl;
+	chmod("private.key", S_IRUSR | S_IWUSR | S_IXUSR); // 700
+	std::ofstream file;
+	file.open("start.sh", std::ios::out | std::ios::app);
+	file << "#!/bin/sh" << std::endl;
+	std::cout << "Create start script. Using screen [YES/NO]? ";
+	bool is_screen = false;
+	if (read_yes())
+	{
+		is_screen = true;
+		file << "screen -S launchserver java -javaagent:LaunchServer.jar -jar LaunchServer.jar";
+	}
+	else file << "java -javaagent:LaunchServer.jar -jar LaunchServer.jar";
+	file << std::endl;
+	file.close();
+	chmod("start.sh", S_IRUSR | S_IWUSR | S_IXUSR); // 700 Что бы не запускали от рута
+	std::cout << "Start script created!" << std::endl;
+	if (is_root)
+	{
+		std::cout << "Switch to the user \"launchserver\"(su - launchserver) before using start.sh" << std::endl;
+	}
+	if (is_screen)
+	{
+		std::cout << "Screen mini help:" << std::endl;
+		std::cout << "\"screen -x\" - switch to the last screen" << std::endl;
+		std::cout << "\"Ctrl + A + D\" - exit screen" << std::endl;
+	}
+	return 0;
+}
+const char* avalible_options = "bm";
+struct installerOptions
+{
+	bool isBuild = false;
+	bool isNoInstall = false;
+};
+installerOptions opts;
+int main(int argc, char **argv)
+{
+	int opt; // каждая следующая опция попадает сюда
+	while ((opt = getopt(argc, argv, avalible_options)) != -1) { // вызываем getopt пока она не вернет -1
+		switch (opt) {
+		case 'b': // Собирать из исходников
+			opts.isBuild = true;
+			break;
+		case 'm': // Пропустить этап установки(например для установки модулей в уже установленный LaunchServer)
+			opts.isNoInstall = true;
+			break;
+		}
+	}
+	std::cout << "GravitLauncher Linux installer" << std::endl;
+	if (is_file_exist("/bin/apt-get"))
+	{
+		is_debian = true;
+	}
+	if (is_file_exist("/bin/pacman"))
+	{
+		is_arch = true;
+	}
+	if (is_file_exist("/bin/yum"))
+	{
+		is_centos = true;
+	}
+	if (getuid() == 0)
+	{
+		if (root_action() != 0) return -1;
+	}
+	std::cout << "Avalible versions: [dev, 5.0.0b6, 5.0.0b7]" << std::endl;
+	std::cout << "Print version:";
+	std::string version;
+	std::cin >> version;
+	if (version == "dev" || version == "5.0.0b6" || version == "5.0.0b7")
+	{
+		download_url = download_url + version;
+	}
+	else
+	{
+		std::cout << "Version " << version << " not found. Download dev[YES/NO]?";
+		if (read_yes())
+		{
+			download_url = download_url + "dev";
+		}
+		else download_url = download_url + version;
+	}
+	if (!opts.isNoInstall && install_action() != 0) return -1;
+	std::cout << "Download modules[YES/NO]?";
+	if (read_yes())
+	{
+		if (is_file_exist("modules"))
+			mkdir("modules", S_IRUSR | S_IWUSR | S_IXUSR);
+		chdir("modules");
+		std::string module;
+		std::cout << "Print module name:";
+		std::cin >> module;
+		while (module != "" && module != "stop" && module != "exit")
+		{
+			std::string download_module = "curl -o \"" + module + ".jar\" \"" + download_url + "/modules/" + module + "_module.jar\"";
+			std::cout << "Download " << module << ".jar" << std::endl;
+			int ret = system(download_module.c_str());
+			if (ret != 0)
+			{
+				std::cerr << "[ERROR] Error download module " << module << std::endl;
+				if (is_file_exist((module + ".jar").c_str()))
+				{
+					remove((module + ".jar").c_str());
+				}
+			}
+			std::cout << "Print module name:";
+			std::cin >> module;
+		}
+		chdir("..");
+	}
+	if (opts.isBuild) {
+		buildDev();
+	}
+	if (is_root)
+	{
+		if (is_file_exist("/bin/bash"))
+		{
+			execl("/bin/bash", NULL);
+		}
+	}
+}
 
 int buildDev() {
-	cout << "Download dev branch" << endl;
+	std::cout << "Download dev branch" << std::endl;
 	system("git clone --branch=dev https://github.com/GravitLauncher/Launcher.git source");
 	system("cd source");
 	system("rm -r Radon");
-	cout << "Download radon" << endl;
+	std::cout << "Download radon" << std::endl;
 	system("git clone https://github.com/GravitLauncher/Radon.git Radon");
 	system("chmod +x gradlew");
-	cout << "Build" << endl;
+    std::cout << "Build" << std::endl;
 	system("./gradlew build");
 	system("mkdir ../LaunchServer");
 	system("mv LaunchServer/build /libs/* ../LaunchServer");
@@ -173,66 +273,4 @@ int buildDev() {
 	system("rm -rf source");
 	system("cd LaunchServer");
 	test();
-}
-
-int install_action()
-{
-	cout << "UID: " << getuid() << "" << endl;
-	cout << "Download LaunchServer" << endl;
-	string download_launchserver = "curl -o LaunchServer.jar " + download_url + "/LaunchServer.jar";
-	system(download_launchserver.c_str());
-	if (!is_file_exist("LaunchServer.jar"))
-	{
-		cerr << "[ERROR] LaunchServer.jar not found. May be curl not installed?";
-		return -1;
-	}
-	cout << "Download libraries" << endl;
-	string download_lib = "curl -o libraries.zip " + download_url + "/libraries.zip";
-	system(download_lib.c_str());
-	if (!is_file_exist("libraries.zip"))
-	{
-		cerr << "[ERROR] libraries.zip not found. May be curl not installed?";
-		return -1;
-	}
-	cout << "Unpack libraries" << endl;
-	system("unzip libraries.zip");
-	if (!is_file_exist("libraries"))
-	{
-		cerr << "[ERROR] libraries not found. May be unzip not installed?";
-		return -1;
-	}
-	system("rm libraries.zip");
-	test();
-}
-int test() {
-    cout << "Installation test" << endl;
-    system("java -javaagent:LaunchServer.jar -jar LaunchServer.jar multi build checkInstall stop");
-    cout << "Chmod private.key to 700" << endl;
-    chmod("private.key", S_IRUSR | S_IWUSR | S_IXUSR); // 700
-    ofstream file;  
-    file.open ("start.sh", ios::out | ios::app);
-    file << "#!/bin/sh" << endl;
-    cout << "Create start script. Using screen [YES/NO]? ";
-    bool is_screen = false;
-    if(read_yes())
-    {
-        is_screen = true;
-        file << "screen -S launchserver java -javaagent:LaunchServer.jar -jar LaunchServer.jar";
-    }
-    else file << "java -javaagent:LaunchServer.jar -jar LaunchServer.jar";
-    file << endl;
-    file.close();
-    chmod("start.sh", S_IRUSR | S_IWUSR | S_IXUSR); // 700 Что бы не запускали от рута
-    cout << "Start script created!" << endl;
-    if(is_root)
-    {
-        cout << "Switch to the user \"launchserver\"(su - launchserver) before using start.sh" << endl;
-    }
-    if(is_screen)
-    {
-        cout << "Screen mini help:" << endl;
-        cout << "\"screen -x\" - switch to the last screen" << endl;
-        cout << "\"Ctrl + A + D\" - exit screen" << endl;
-    }
-    return 0;
 }
